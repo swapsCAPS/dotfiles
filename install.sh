e99d58191aba4dd9689231d1ea9e48de12909167
swapsCAPS <verstegen.daan@gmail.com>
2017-10-06 00:52:29 +0200

    redshift

--- install.sh
+++ install.sh
@@ -5,10 +5,12 @@ if [ "$CONT" = "y" ]; then
   ln -sf "$(pwd)/.editorconfig" ~/.editorconfig
   ln -sf "$(pwd)/.tern-project" ~/.tern-project
   ln -sf "$(pwd)/libinput-gestures.conf" ~/.config/libinput-gestures.conf
+  ln -sf "$(pwd)/redshift.conf" ~/.config/redshift.conf
 else
   ln -s "$(pwd)/.aliases" ~/.aliases
   ln -s "$(pwd)/.zshrc" ~/.zshrc
   ln -s "$(pwd)/.editorconfig" ~/.editorconfig
   ln -s "$(pwd)/.tern-project" ~/.tern-project
   ln -s "$(pwd)/libinput-gestures.conf" ~/.config/libinput-gestures.conf
+  ln -s "$(pwd)/redshift.conf" ~/.config/redshift.conf
 fi
