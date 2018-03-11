5ae6bc54bcae2934b088904861998b93ba0446a5
swapsCAPS <verstegen.daan@gmail.com>
2018-03-11 14:17:09 +0100

    ¯\_(ツ)_/¯

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ if [ "$CONT" = "y" ]; then
   ln -sf "$(pwd)/.aliases" ~/.aliases
   ln -sf "$(pwd)/.zshrc" ~/.zshrc
   ln -sf "$(pwd)/.editorconfig" ~/.editorconfig
+  ln -sf "$(pwd)/.eslintrc.js" ~/.eslintrc.js
   ln -sf "$(pwd)/.tern-project" ~/.tern-project
   ln -sf "$(pwd)/libinput-gestures.conf" ~/.config/libinput-gestures.conf
   ln -sf "$(pwd)/redshift.conf" ~/.config/redshift.conf
@@ -10,6 +11,7 @@ else
   ln -s "$(pwd)/.aliases" ~/.aliases
   ln -s "$(pwd)/.zshrc" ~/.zshrc
   ln -s "$(pwd)/.editorconfig" ~/.editorconfig
+  ln -s "$(pwd)/.eslintrc.js" ~/.eslintrc.js
   ln -s "$(pwd)/.tern-project" ~/.tern-project
   ln -s "$(pwd)/libinput-gestures.conf" ~/.config/libinput-gestures.conf
   ln -s "$(pwd)/redshift.conf" ~/.config/redshift.conf
