fa3659a0290ce35f25598109c3bcc2b19f127d77
swapsCAPS <verstegen.daan@gmail.com>
2018-08-12 00:13:18 +0200

    things

--- install-global-npm-packages.sh
+++ install-global-npm-packages.sh
@@ -4,10 +4,11 @@ npm install -g \
   jsonlint \
   coffeelint \
   coffeelint-cjsx \
+  coffeescript@2 \
+  coffeescope2 \
   tern \
   nodemon \
   coffee-script \
   eslint-plugin-import \
   eslint-plugin-jsx-a11y \
-  eslint-plugin-react \
-  coffeescript@2
+  eslint-plugin-react
