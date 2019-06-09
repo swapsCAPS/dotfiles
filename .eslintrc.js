e422df90c7fed771c5dfdfcb9d8246ca9deb153d
swapsCAPS <verstegen.daan@gmail.com>
2019-06-09 11:20:35 +0200

    bla

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,15 @@
 module.exports = {
   extends: [ 'standard', 'prettier' ],
   plugins: ['prettier', 'align-assignments' ],
-  rules: {
-    'prettier/prettier': 'error',
-  },
-  env: { browser: true, node: true, mocha: true },
-  rules: {
-    'comma-dangle':          ['warn', 'always'],
-    'no-unused-vars':        ['warn', { vars: 'all' }],
-    'no-multi-spaces':       ['warn', {
-
-      'exceptions': {
-        'ImportDeclaration':  true,
-        'VariableDeclarator': true
-      }
-    }],
-
-    'key-spacing':           ['warn', {
-      "align": 'value'
-    }],
-
-    'padded-blocks':         ['warn', 'never'],
-    'array-bracket-spacing': ['warn', 'always'],
+  env:     { browser: true, node: true, mocha: true },
+  rules:   {
+    'array-bracket-spacing':               ['warn', 'always'],
+    'comma-dangle':                        ['warn', 'always'],
+    'key-spacing':                         ['warn', { "align": 'value' }],
+    'no-unused-vars':                      ['warn', { vars: 'all' }],
+    'no-multi-spaces':                     ['warn', { 'exceptions': { 'ImportDeclaration':  true, 'VariableDeclarator': true } }],
+    'padded-blocks':                       ['warn', 'never'],
+    'prettier/prettier':                   'error',
     'align-assignments/align-assignments': ['warn', { 'requiresOnly': false } ]
   },
 }
