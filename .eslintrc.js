64db719a04586e7c1f0abcd752b3d0867ee04d66
swapsCAPS <verstegen.daan@gmail.com>
2019-06-09 11:38:49 +0200

    moar things

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,11 @@ module.exports = {
   env:     { browser: true, node: true, mocha: true },
   rules:   {
     'array-bracket-spacing':               ['warn', 'always'],
-    'comma-dangle':                        ['warn', 'always'],
+    'comma-dangle':                        ['warn', 'always-multiline'],
     'key-spacing':                         ['warn', { "align": 'value' }],
     'no-unused-vars':                      ['warn', { vars: 'all' }],
-    'no-multi-spaces':                     ['warn', { 'exceptions': { 'ImportDeclaration':  true, 'VariableDeclarator': true } }],
+    'no-multi-spaces':                     'off',
     'padded-blocks':                       ['warn', 'never'],
-    'align-assignments/align-assignments': ['warn', { 'requiresOnly': false } ]
+    'align-assignments/align-assignments': ['warn', { 'requiresOnly': true } ]
   },
 }
