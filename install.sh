ded09dd8c94810518a6171161dbd623a9edaffeb
swapsCAPS <verstegen.daan@gmail.com>
2019-12-06 14:13:25 +0100

    alacritty + sway updates

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 read -p "Symlink using -f (y/n)?" CONT
+mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/sway
 if [ "$CONT" = "y" ]; then
+  ln -sf "$(pwd)/swayConf" ~/.config/sway/config
+  ln -sf "$(pwd)/alacritty.yml" ~/.config/alacritty/alacritty.yml
   ln -sf "$(pwd)/.aliases" ~/.aliases
   ln -sf "$(pwd)/.zshrc" ~/.zshrc
   ln -sf "$(pwd)/.editorconfig" ~/.editorconfig
@@ -8,6 +12,9 @@ if [ "$CONT" = "y" ]; then
   ln -sf "$(pwd)/libinput-gestures.conf" ~/.config/libinput-gestures.conf
   ln -sf "$(pwd)/redshift.conf" ~/.config/redshift.conf
 else
+  ln -s "$(pwd)/swayConf" ~/.config/sway/config
+  ln -s "$(pwd)/alacritty.yml" ~/.config/alacritty/alacritty.yml
+  ln -s "$(pwd)/." ~/.config/sway/config
   ln -s "$(pwd)/.aliases" ~/.aliases
   ln -s "$(pwd)/.zshrc" ~/.zshrc
   ln -s "$(pwd)/.editorconfig" ~/.editorconfig
