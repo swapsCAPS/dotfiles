c882039e060fc54bed6f30fcf14d6dbf691fda42
swapsCAPS <verstegen.daan@gmail.com>
2022-08-24 11:51:05 +0200

    post debian install script

---
+++ post-debian-install.sh
@@ -0,0 +1,33 @@
+dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:swapescape']"
+
+sudo apt update
+sudo apt --ignore-missing install \
+  autoconf \
+  automake \
+  autotools-dev \
+  bat \
+  build-essential \
+  cmake \
+  curl \
+  fd-find \
+  g++ \
+  gcc \
+  git \
+  git-flow \
+  htop \
+  lynx \
+  mc \
+  neovim \
+  ripgrep \
+  ssh \
+  tmux \
+  tmuxinator \
+  unzip \
+  wget \
+  zsh
+
+git config --global user.name "swapsCAPS"
+git config --global user.email "verstegen.daan@gmail.com"
+
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
