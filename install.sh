852661e1c92beffa63733c0552ef7edd7f243587
swapsCAPS <verstegen.daan@gmail.com>
2025-07-06 22:37:29 +0200

    add mise.toml

--- install.sh
+++ install.sh
@@ -1,25 +1,15 @@
 read -p "Symlink using -f (y/n)?" CONT
-mkdir -p ~/.config/alacritty
-mkdir -p ~/.config/sway
+mkdir -p ~/.config
 if [ "$CONT" = "y" ]; then
-  ln -sf "$(pwd)/swayConf" ~/.config/sway/config
-  ln -sf "$(pwd)/alacritty.yml" ~/.config/alacritty/alacritty.yml
   ln -sf "$(pwd)/.aliases" ~/.aliases
   ln -sf "$(pwd)/.zshrc" ~/.zshrc
   ln -sf "$(pwd)/.editorconfig" ~/.editorconfig
   ln -sf "$(pwd)/.eslintrc.js" ~/.eslintrc.js
-  ln -sf "$(pwd)/.tern-project" ~/.tern-project
-  ln -sf "$(pwd)/libinput-gestures.conf" ~/.config/libinput-gestures.conf
-  ln -sf "$(pwd)/redshift.conf" ~/.config/redshift.conf
+  ln -sf "$(pwd)/mise.toml" ~/.config/.
 else
-  ln -s "$(pwd)/swayConf" ~/.config/sway/config
-  ln -s "$(pwd)/alacritty.yml" ~/.config/alacritty/alacritty.yml
-  ln -s "$(pwd)/." ~/.config/sway/config
   ln -s "$(pwd)/.aliases" ~/.aliases
   ln -s "$(pwd)/.zshrc" ~/.zshrc
   ln -s "$(pwd)/.editorconfig" ~/.editorconfig
   ln -s "$(pwd)/.eslintrc.js" ~/.eslintrc.js
-  ln -s "$(pwd)/.tern-project" ~/.tern-project
-  ln -s "$(pwd)/libinput-gestures.conf" ~/.config/libinput-gestures.conf
-  ln -s "$(pwd)/redshift.conf" ~/.config/redshift.conf
+  ln -s "$(pwd)/mise.toml" ~/.config/.
 fi
